a649f8f9f6b0c107be8e48f60e1aeb05fb50ea84
Brock Tillotson <brock.m.tillotson@gmail.com>
2017-05-02 11:36:07 -0700

    done! yay it works, could use some function cleanup, but ready to move on to ruby again

--- js/script.js
+++ js/script.js
@@ -1,20 +1,30 @@
 $(document).ready(function(){
   var clicked = 0;
   var interval;
-  var secs = (10);
-  $("p").text(secs);
+  var secs = (25*60);
+
+  var display = new Date(null);
+  display.setSeconds(secs);
+  display = (String(display)).substring(19,25);
+
+  $("p").text(display);
   var cycle;
   var work = true;
   $(".dir").text("work time!");
 
-
+////////////////////////////////////////////////////
   $("#reset").click(function(){
-    secs = (10);
+    secs = (25*60);
     work = true;
-    $("p").text(secs);
+    var display2 = new Date(null);
+    display2.setSeconds(secs);
+    display2 = (String(display2)).substring(19,25);
+    $("p").text(display2);
     $('.dir').css('background-color', 'yellow');
     $(".dir").text("work time!");
   })
+
+  ////////////////////////////////////////////////
 
   $("#start").click(function(){
     if (clicked % 2 == 0) {
@@ -26,7 +36,10 @@ $(document).ready(function(){
 
         if (secs > 0){
         secs -= 1;
-        $("p").text(secs);
+        var display3 = new Date(null);
+        display3.setSeconds(secs);
+        display3 = (String(display3)).substring(19,25);
+        $("p").text(display3);
         } else {
           if (work == false) {
             secs = (10);
@@ -34,7 +47,7 @@ $(document).ready(function(){
             $('.dir').css('background-color', 'yellow');
             $(".dir").text("work time!");
           } else{
-            secs = (5);
+            secs = (5*60);
             work = false;
             $('.dir').css('background-color', 'green');
             $(".dir").text("break time!");
@@ -44,9 +57,7 @@ $(document).ready(function(){
 
       cycle = setInterval(function(){elapse() }, 1000);
 
-      $("p").text(elapsedTime);
-
-
+      
     } else {
       $("#start").text("START");
       clicked += 1;
@@ -54,11 +65,8 @@ $(document).ready(function(){
     }
   })
 
+//////////////////////////////////////////////
 
-
-  $("#pause").click(function(){
-
-    $("p").toggleClass("red");
-  })
+  
 
 });
